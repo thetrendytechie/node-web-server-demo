c3b69dd7981791e8b21c4137904a2fa2df7b3117
Sage Franch <trendytechie@live.com>
2018-07-23 10:29:00 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello express</h1>');
+  // res.send({
+  //   name: 'Sage',
+  //   likes: [
+  //     'coffee',
+  //     'boots'
+  //   ]
+  // });
+  res.render('home.hbs', {
+    pageTitle: `Sage's Home Pages`,
+    welcomeMessage: 'Welcome to the future',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad Request'
+  });
+});
+
+// app.get('/help', (req, res) => {
+//   res.send('/public/help.html');
+// });
+
+app.listen(3000, () => {
+  console.log('Server up on port 3000');
+});
